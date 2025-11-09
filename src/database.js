8a16c18795c2454478476978e989de9437f2ebc5
William Sinchi <45720107+william9969@users.noreply.github.com>
2025-11-09 13:01:07 -0500

    fix: Se elimina archivo dabase

--- src/database.js
+++
@@ -1,5 +0,0 @@
-const mongoose = require('mongoose');
-const URI='mongodb://127.0.0.1/usuarios_db';
-mongoose.connect(URI).then(db=> console.log('BD conectada')).catch(err => console.error(err));
-
-module.exports=mongoose;
