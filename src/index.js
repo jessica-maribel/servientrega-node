efb9a397ef89985bb8be15d28567eb5a96de2f86
William Sinchi <45720107+william9969@users.noreply.github.com>
2025-11-09 13:01:38 -0500

    fix: Se elimina archivo index

--- src/index.js
+++
@@ -1,6 +0,0 @@
-require('./database');
-const app= require('./app')
-app.listen(app.get('puerto'), ()=>{
- console.log('Nombre de la App',app.get('nombreApp'));
- console.log('Puerto del servidor',app.get('puerto'));
-})
